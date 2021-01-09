f266cb680b4713c5261aa181837e697810b5bca7
Tamtnguye <apple@Apples-MacBook-Pro.local>
2021-01-09 15:00:21 -0800

    pathways

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const path = require("path");
 const PORT = 5001
 const app = express();
 
+app.use("/static", express.static(path.resolve(__dirname,"frontend", "static")));
+
 app.get("/*", (req, res) => {
-    res.sendFile(path.resolve("index.html"));
+    res.sendFile(path.resolve(__dirname,"frontend","html","home.html"));
 });
 
 app.listen(process.env.PORT || 5001, () => console.log(`Server Running at ${PORT}`));
