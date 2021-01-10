91e91f605b8e06391180a039e8ba4c53e3f80b69
Tamtnguye <apple@Apples-MacBook-Pro.local>
2021-01-10 05:01:42 -0800

    paths for index

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(express.static("frontend"));
 
 app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname,"html","index.html"));
+});
+
+app.get("/home", (req, res) => {
     res.sendFile(path.resolve(__dirname,"html","home.html"));
 });
 
