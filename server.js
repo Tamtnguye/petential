ff9d8819d8cb1c59632b89563f8504763519d7b0
Tamtnguye <apple@Apples-MacBook-Pro.local>
2021-01-09 16:56:03 -0800

    redo layout

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use("/static", express.static(path.resolve(__dirname,"frontend", "static")));
 
 app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname,"frontend","html","home.html"));
+    res.sendFile(path.resolve(__dirname,"html","home.html"));
 });
 
 app.listen(process.env.PORT || 5001, () => console.log(`Server Running at ${PORT}`));
